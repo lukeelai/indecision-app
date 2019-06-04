7b56c3a66a7e4f92342c043d4561610a71c45af7
Luke Lai <LukeL@meyer.com>
2019-06-04 10:02:55 -0700

    ES6 Classes: Part 2

--- src/playground/es6-class1.js
+++ src/playground/es6-class1.js
@@ -14,10 +14,44 @@ class Person {
   }
 }
 
-const me = new Person("Luke Lai", 24);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+    return description;
+  }
+}
+
+class Travler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+    if (this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}`;
+    }
+    return greeting;
+  }
+}
+
+const me = new Travler("Luke Lai", 24, "Cerritos");
 console.log(me.getGreeting());
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Travler();
 console.log(other.getGreeting());
 console.log(other.getDescription());
