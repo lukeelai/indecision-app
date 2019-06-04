ac8612e5d1e1b333cdff501b19561134d3a52c1d
Luke Lai <LukeL@meyer.com>
2019-06-04 09:41:19 -0700

    ES6 Classes: Part 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,39 +1,40 @@
-'use strict';
+"use strict";
 
-var visibility = false;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var toggleVisibility = function toggleVisibility() {
-    visibility = !visibility;
-    renderApplication();
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var appRoot = document.getElementById('app');
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Anonymous";
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var renderApplication = function renderApplication() {
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Generic Title'
-        ),
-        React.createElement(
-            'button',
-            { onClick: toggleVisibility },
-            visibility ? 'Hide Details' : 'Show Details'
-        ),
-        visibility && React.createElement(
-            'div',
-            null,
-            React.createElement(
-                'p',
-                null,
-                'details'
-            )
-        )
-    );
-    ReactDOM.render(template, appRoot);
-};
+    _classCallCheck(this, Person);
 
-renderApplication();
+    this.name = name;
+    this.age = age;
+  }
+
+  _createClass(Person, [{
+    key: "getGreeting",
+    value: function getGreeting() {
+      // return "Hi, I am " + this.name + "!";
+      return "Hi I am " + this.name + "!";
+    }
+  }, {
+    key: "getDescription",
+    value: function getDescription() {
+      return this.name + " is " + this.age + " year(s) old.";
+    }
+  }]);
+
+  return Person;
+}();
+
+var me = new Person("Luke Lai", 24);
+console.log(me.getGreeting());
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getGreeting());
+console.log(other.getDescription());
