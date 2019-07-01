167c7eb9bac6a7496f3a8cc4a645e0510cdbc73d
Luke Lai <LukeL@meyer.com>
2019-07-01 08:56:23 -0700

    51. Import and Export

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,35 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log("webpack test");
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
+// import "../utils.js";
+// import { square, add } from "../utils.js";
+
+// console.log("webpack test");
+// console.log(square(4));
+// console.log(add(2, 3));
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](18));
+
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const isAdult = age => age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+
+const canDrink = age => age >= 21;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
+
 
 
 /***/ })
--- src/playground/app.js
+++ src/playground/app.js
@@ -1,1 +1,10 @@
-console.log("webpack test");
+// import "../utils.js";
+// import { square, add } from "../utils.js";
+
+// console.log("webpack test");
+// console.log(square(4));
+// console.log(add(2, 3));
+
+import { canDrink, isAdult } from "../person.js";
+console.log(canDrink(18));
+console.log(isAdult(18));
